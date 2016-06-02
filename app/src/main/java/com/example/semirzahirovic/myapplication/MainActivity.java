fed7c9c428dfec90c9815ab3115776a891e2183b
semirzahirovic <semir.zahirovic@live.com>
2016-06-02 17:48:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/semirzahirovic/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.semirzahirovic.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.mylibrary.SweetAlertDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
